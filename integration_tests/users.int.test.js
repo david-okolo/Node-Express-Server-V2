bad06c1b571daf0e6f36b15d8958aa9d9c61706c
David Okolo <david.okolo@icloud.com>
2019-07-03 14:58:40 +0100

    Fixed user api interation test

--- integration_tests/users.int.test.js
+++ integration_tests/users.int.test.js
@@ -58,148 +58,160 @@ describe('Users API tests', ()=>{
         })
     });
 
-    describe(`Registration`, ()=>{
-        it(`should not register bad requests`, (done) => {
-            let req = http.request(createOptions('register'), (res) => {
-                done()
-                res.on('data', (result)=>{
-                    result = JSON.parse(result);
-                    expect(result.success).to.be.false
-                    expect(result.msg).to.equal('user validation failed')
+    
+    describe(`Tests start`, ()=>{
+        describe(`Registration`, ()=>{
+            it(`should not register bad requests`, (done) => {
+                let request = http.request(createOptions('register'), (response) => {
+                    response.on('data', (result)=>{
+                        result = JSON.parse(result);
+                        expect(result.success).to.be.false
+                        expect(result.msg).to.equal('user validation failed');
+                        done();
+                    });
                 });
-            });
-            let body = {
-                name: "root"
-            }
-            req.write(JSON.stringify(body));
-            req.end();
-        })
-
-        it('should register new user request',(done)=>{
-            let req = http.request(createOptions('register'), (res)=>{
-                done();
-                res.on('error', (err)=>{
-                    console.log('Err: ' +err);
+                let body = {
+                    name: "root"
+                }
+                request.write(JSON.stringify(body));
+                request.end();
+            })
+    
+            it('should register new user request',(done)=>{
+                let request = http.request(createOptions('register'), (response)=>{
+                    
+                    response.on('error', (err)=>{
+                        console.log('Err: ' +err);
+                        done();
+                    });
+                    response.on('data', (result)=>{
+                        result = JSON.parse(result);
+                        expect(result.data).to.contain.property('_id');
+                        expect(result.data).to.contain.property('username');
+                        expect(result.data).to.contain.property('password');
+                        expect(result.data).to.contain.property('email');
+                        expect(result.success).to.be.true
+                        done();
+                    });
                 });
-                res.on('data', (result)=>{
-                    result = JSON.parse(result);
-                    expect(result.data).to.contain.property('_id');
-                    expect(result.data).to.contain.property('username');
-                    expect(result.data).to.contain.property('password');
-                    expect(result.data).to.contain.property('email');
-                    expect(result.success).to.be.true
+                request.write(JSON.stringify(createUser()));
+                request.end();
+            });  
+    
+            it(`should not register an existing account with the same email or username`, (done)=>{
+                let request = http.request(createOptions('register'), (response)=>{
+                    
+                    response.on('error', (err) => {
+                        console.log(err)
+                        done();
+                    });
+                    response.on('data', (result)=>{
+                        result = JSON.parse(result);
+                        expect(result.success).to.equal(false)
+                        expect(result.msg).to.equal('email already exists' || 'username already exists')
+                        done();
+                    })
                 });
-            });
-            req.write(JSON.stringify(createUser()));
-            req.end();
-        });  
-
-        it(`should not register an existing account with the same email or username`, (done)=>{
-            let req = http.request(createOptions('register'), (res)=>{
-                done();
-                res.on('error', (err) => {
-                    console.log(err)
-                });
-                res.on('data', (result)=>{
-                    result = JSON.parse(result);
-                    expect(result.success).to.equal(false)
-                    expect(result.msg).to.equal('email already exists' || 'username already exists')
-                })
-            });
-            req.write(JSON.stringify(createUser()));
-            req.end();
-        })
-    });
-
-    describe(`Authentication`, ()=>{
-        it(`should reject bad request`, (done)=>{
-            let req = http.request(createOptions('/authenticate'), (res)=>{
-                res.on('error', (err)=>{
-                    console.log(err)
-                })
-                res.on('data', (result)=>{
-                    result = JSON.parse(result);
-                    expect(result.success).to.be.false;
-                    expect(result.msg).to.equal('bad request');
-                });
-                done();
-            });
-
-            let body = {
-                name: 'Cookie'
-            }
-            req.write(JSON.stringify(body));
-            req.end();
+                request.write(JSON.stringify(createUser()));
+                request.end();
+            })
         });
-
-        it(`should accept and authenticate correct username`, (done)=>{
-            let req = http.request(createOptions('/authenticate'), (res)=>{
-                res.on('error', (err)=>{
-                    console.log(err)
-                    done()
+    
+        describe(`Authentication`, ()=>{
+            it(`should reject bad request`, (done)=>{
+                let request = http.request(createOptions('authenticate'), (response)=>{
+                    
+                    response.on('error', (err)=>{
+                        console.log(err)
+                        done();
+                    })
+                    response.on('data', (result)=>{
+                        result = JSON.parse(result);
+                        expect(result.success).to.be.false;
+                        expect(result.msg).to.equal('bad request');
+                        done();
+                    });
                 });
-                res.on('data', (result)=>{
-                    result = JSON.parse(result);
-                    expect(result.success).to.be.true;
-                    expect(result.data).to.contain.property('token');
-                    done();
-                });
+    
+                let body = {
+                    name: 'Cookie'
+                }
+                request.write(JSON.stringify(body));
+                request.end();
             });
-            req.write(JSON.stringify(createLogin()))
-            res.end()
-        });
-
-        it(`should accept and authenticate correct email`, (done)=>{
-            let req = http.request(createOptions('/authenticate'), (res)=>{
-                res.on('error', (err)=>{
-                    console.log(err)
-                    done()
+    
+            it(`should accept and authenticate correct username`, (done)=>{
+                let request = http.request(createOptions('authenticate'), (response)=>{
+                    
+                    response.on('error', (err)=>{
+                        console.log(err)
+                        done();
+                    });
+                    response.on('data', (result)=>{
+                        result = JSON.parse(result);
+                        expect(result.success).to.be.true;
+                        expect(result.data).to.contain.property('token');
+                        done();
+                    });
                 });
-                res.on('data', (result)=>{
-                    result = JSON.parse(result);
-                    expect(result.success).to.be.true;
-                    expect(result.data).to.contain.property('token');
-                    done();
-                });
+                
+                request.write(JSON.stringify(createLogin()));
+                request.end()
             });
-            req.write(JSON.stringify(createLogin(email)));
-            res.end();
-        });
-
-        it(`should reject authentication with wrong password`, (done)=>{
-            let req = http.request(createOptions('/authenticate'), (res)=>{
-                res.on('error', (err)=>{
-                    console.log(err)
-                    done()
+    
+            it(`should accept and authenticate correct email`, (done)=>{
+                
+                let request = http.request(createOptions('authenticate'), (response)=>{
+                    response.on('error', (err)=>{
+                        console.log(err)
+                        done()
+                    });
+                    response.on('data', (result)=>{
+                        result = JSON.parse(result);
+                        expect(result.success).to.be.true;
+                        expect(result.data).to.contain.property('token');
+                        done();
+                    });
                 });
-                res.on('data', (result)=>{
-                    result = JSON.parse(result);
-                    expect(result.success).to.be.false;
-                    expect(result.msg).to.equal('wrong password')
-                    done();
-                });
+                request.write(JSON.stringify(createLogin(email)));
+                request.end();
             });
-            req.write(JSON.stringify(createLogin(email, 'freak')));
-            res.end();
-        });
-
-        it(`should return user not found`, (done)=>{
-            let req = http.request(createOptions('/authenticate'), (res)=>{
-                res.on('error', (err)=>{
-                    console.log(err)
-                    done()
+    
+            it(`should reject authentication with wrong password`, (done)=>{
+                let request = http.request(createOptions('authenticate'), (response)=>{
+                    response.on('error', (err)=>{
+                        console.log(err)
+                        done()
+                    });
+                    response.on('data', (result)=>{
+                        result = JSON.parse(result);
+                        expect(result.success).to.be.false;
+                        expect(result.msg).to.equal('wrong password')
+                        done();
+                    });
                 });
-                res.on('data', (result)=>{
-                    result = JSON.parse(result);
-                    expect(result.success).to.be.false;
-                    expect(result.msg).to.equal('user doesn\'t exist');
-                    done();
-                });
+                request.write(JSON.stringify(createLogin(email, 'freak')));
+                request.end();
             });
-            req.write(JSON.stringify(createLogin('drake')));
-            res.end();
+    
+            it(`should return user not found`, (done)=>{
+                let request = http.request(createOptions('authenticate'), (response)=>{
+                    response.on('error', (err)=>{
+                        console.log(err)
+                        done()
+                    });
+                    response.on('data', (result)=>{
+                        result = JSON.parse(result);
+                        expect(result.success).to.be.false;
+                        expect(result.msg).to.equal('user doesn\'t exist');
+                        done();
+                    });
+                });
+                request.write(JSON.stringify(createLogin('drake@icloud.com')));
+                request.end();
+            });
         });
-    });
+    })
 
     after((done)=>{
         dbclose().then(()=>{
