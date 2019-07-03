7df673c4726201db4d5f5ab9b0e2ef25d72e48d0
David Okolo <david.okolo@icloud.com>
2019-07-03 10:26:15 +0100

    Completed register route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,18 +2,24 @@ import user from '../models/user.model'
 import bcrypt from 'bcrypt';
 
 
-export const addUser = (_newUser, callback)=>{
+const addUser = (_newUser, callback)=>{
     bcrypt.hash(_newUser.password, 10, (err, hash)=>{
         _newUser.password = hash;
         user.create(_newUser, callback)
     });
 }
 
-export const getUserByUsername = (_username, callback)=>{
+const getUserByUsername = (_username, callback)=>{
     user.findOne({username: _username}, callback)
 }
 
+const getUserByEmail = (_email, callback)=>{
+    user.findOne({email: _email}, callback)
+}
+
+
 export default {
     addUser,
-    getUserByUsername
+    getUserByUsername,
+    getUserByEmail
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -9,20 +9,62 @@ router.post('/register', (req, res)=>{
         password: req.body.password,
         email: req.body.email
     }
-    UserController.addUser(user, (err, user)=>{
-        if (err) res.status(500).json({
-            success: false,
-            msg: err._message
-        })
 
-        if(user){
-            res.status(200).json({
-                success: true,
-                data: user,
-                msg: "succesfully added"
+    UserController.getUserByEmail(user.email, (err, user)=>{
+        if(err){
+            res.json({
+                success: false,
+                msg: err._message
+            })
+        } 
+
+        if(!user){
+
+            UserController.getUserByUsername(user.username, (err, user)=>{
+                if(err){
+                    res.json({
+                        success: false,
+                        msg: err._message
+                    })
+                }
+
+                if(!user){
+
+                    UserController.addUser(user, (err, user)=>{
+                        if (err) res.status(500).json({
+                            success: false,
+                            msg: err._message
+                        })
+                
+                        if(user){
+                            res.status(200).json({
+                                success: true,
+                                data: user,
+                                msg: "succesfully added"
+                            })
+                        }
+                    });
+
+                }else{
+
+                    res.status(402).json({
+                        success: false,
+                        msg: 'username already exists'
+                    })
+                    
+                }
+
+
+            })
+            
+        }else{
+            res.status(402).json({
+                success: false,
+                msg: 'email already exists'
             })
         }
-    });
+
+    })
 })
 
 export default router;
