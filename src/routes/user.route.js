c63e74f89e945c966369d5fa4ebf1e3b67af402f
David Okolo <david.okolo@icloud.com>
2019-07-08 17:08:46 +0100

    changed function declaration to expression

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,7 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import config from '../../config';
 
-function isEmail(_query){
+const isEmail = (_query)=>{
     let patternEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return patternEmail.test(_query);
 }
